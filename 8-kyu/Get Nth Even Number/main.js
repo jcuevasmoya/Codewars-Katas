dda25e7719a02ff5d239117ccd62e29388b06517
Javier Cuevas <jay.cuevas1@gmail.com>
2021-04-12 21:24:58 -0400

    update

--- 8-kyu/Get Nth Even Number/main.js
+++ 8-kyu/Get Nth Even Number/main.js
@@ -9,5 +9,12 @@
 
 
 function nthEven(n){
-return ()
+return n * 2 - 2
 }
+
+console.log(nthEven(1), 0, "Wrong Value!");
+console.log(nthEven(2), 2, "Wrong Value!");
+console.log(nthEven(3), 4, "Wrong Value!");
+
+console.log(nthEven(100), 198, "Wrong Value!");
+console.log(nthEven(1298734), 2597466, "Wrong Value!");
