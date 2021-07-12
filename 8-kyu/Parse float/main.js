af83df92f123848ec5c6bdc38baf43ab23695a09
Javier Cuevas <jay.cuevas1@gmail.com>
2021-07-11 23:55:30 -0400

    update

--- 8-kyu/Parse float/main.js
+++ 8-kyu/Parse float/main.js
@@ -1,7 +1,5 @@
 // Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
 
-function parseF(s) {
-  return parseInt(s);
-}
+const parseF = s => (Number.isNaN(parseFloat(s)) ? null : parseFloat(s))
 
 console.log(parseF("1"), 1.0)
