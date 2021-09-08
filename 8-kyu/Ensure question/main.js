589aa97d89e3d0dfad948c25f96ccf335ce2ed7f
Javier Cuevas <jay.cuevas1@gmail.com>
2021-09-08 00:21:23 -0400

    new kata

---
+++ 8-kyu/Ensure question/main.js
@@ -0,0 +1,23 @@
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+// For example (Input --> Output)
+
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+
+function ensureQuestion(s) {
+  if(s.substr(-1) === '?'){
+    return s
+  }else{
+    return `${s}?`
+  }
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+
+console.log(ensureQuestion(""),"?","Expected: '?'");
+console.log(ensureQuestion("Yes"),"Yes?","Expected: '?'");
+console.log(ensureQuestion("No?"),"No?","Expected: '?'");
