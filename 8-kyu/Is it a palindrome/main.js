e2aa5f284684bd1dab728fb52a59b7d7f554d57f
Javier Cuevas <jay.cuevas1@gmail.com>
2021-09-25 21:38:17 -0400

    update

--- 8-kyu/Is it a palindrome/main.js
+++ 8-kyu/Is it a palindrome/main.js
@@ -4,8 +4,6 @@ function isPalindrome(x){
   return x.toLowerCase() === x.toLowerCase().split('').reverse().join('')
 }
 
-
-
 console.log(isPalindrome("a"), true)
 console.log(isPalindrome("aba"), true)
 console.log(isPalindrome("Abba"), true)
@@ -14,4 +12,3 @@ console.log(isPalindrome("Bob"), true)
 console.log(isPalindrome("Madam"), true)
 console.log(isPalindrome("AbBa"), true)
 console.log(isPalindrome(""), true)
-
