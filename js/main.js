6b5e7aee1960c0a8c65b8196b69373943d13b611
Javier Cuevas <jay.cuevas1@gmail.com>
2021-12-11 23:32:30 -0500

    new kata

--- js/main.js
+++ js/main.js
@@ -1,26 +1,8 @@
-function insurance(age, size, numofdays){
-  if(age < 25){
-    age = 10
-  }else{
-    age = 0
-  }
-console.log(age)
-if(size == 'economy'){
-  size = 0
-}else if(size ==='medium'){
-  size = 10
-}else{
-  size = 15
+function addLength(str) {
+  let breakString = str.split(' ')
+  console.log(breakString)
+  breakString.forEach(word => console.log(word.length))
+  console.log(breakString.map(word => `${word} ${word.length}`))
 }
 
-
-console.log(size)
-if(numofdays < 0){
-  numofdays = 0
-}else{
-  numofdays
-}
-    return (age*numofdays)+(size*numofdays)+(numofdays*50)
-  }
-
- console.log(insurance(18, "medium", 7))
+console.log(addLength('you will win'))
