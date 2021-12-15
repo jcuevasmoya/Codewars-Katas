de08bdd8b5885d13583449429653fc7d01dfbeac
Javier Cuevas <jay.cuevas1@gmail.com>
2021-12-14 23:56:03 -0500

    new kata

---
+++ 7-kyu/Basic JS average calculator/main.js
@@ -0,0 +1,19 @@
+// Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+
+// The test expects you to provide a Calculator object with an average method:
+
+// Calculator.average()
+
+// The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+
+// It expects Calculator.average(3,4,5) to return 4.
+
+var Calculator = {
+    average: function(...arr) {
+        return  arr.length == 0 ? 0 : arr.reduce((a,b)=> a+b,0)/arr.length
+    }
+};
+
+console.log(Calculator.average(3,4,5)).to.be.approximately(4, eps);
+console.log(Calculator.average(3,4,5,6,7,8,9,8,7,6,5,4,3)).to.be.approximately(5.769230769230769, eps);
+console.log(Calculator.average()).to.be.equal(0, 'Calculator.average() (no arguments) should result in 0');
