a27a5869083b3ecc2d3f672caeae0f059ab33aa5
Javier Cuevas <jay.cuevas1@gmail.com>
2021-12-23 22:43:35 -0500

    new kata

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-function addLength(str) {
-  let breakString = str.split(' ')
-  console.log(breakString)
-  breakString.forEach(word => console.log(word.length))
-  console.log(breakString.map(word => `${word} ${word.length}`))
+function roundIt(n){
+  let splitNum = n.toString().split('.')
+  let a = parseInt(splitNum[0])
+  let b = parseInt(splitNum[1])
+  
+  console.log(a, b)
+  if(a < b){
+    return Math.ceil(n)
+  }else if (a > b ) {
+    return Math.floor(n)
+  }else {
+    return Math.round(n)
+  }
 }
 
-console.log(addLength('you will win'))
+console.log(roundIt(3.45))
+console.log(roundIt(34.5))
+console.log(roundIt(34.56))
+console.log(roundIt(45169.48271))
+console.log(roundIt(9185.6619))
+console.log(roundIt(257646.426787))
